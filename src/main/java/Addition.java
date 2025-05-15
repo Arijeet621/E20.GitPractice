e719e5cbb5de296f72f0225dba0c62d5902aabf7
Arijeet <arijeetnyctc@gmail.com>
2025-05-15 16:52:23 +0530

    Addition class committed

---
+++ src/main/java/Addition.java
@@ -0,0 +1,14 @@
+
+public class Addition {
+
+	public static void main(String[] args) {
+		
+		int result = add(67,78);
+		System.out.println(result);
+	}
+	public static int add(int a, int b)
+	{
+		return a+b;
+	}
+
+}
