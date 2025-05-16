926c772b8c7c4b7394bf781ba9f1816bd69798ae
Arijeet <Arijeet@Arijeet>
2025-05-16 17:02:11 +0530

    Subtraction

--- src/main/java/objectRepo/Substraction.java
+++
@@ -1,9 +0,0 @@
-package objectRepo;
-
-public class Substraction {
-
-	public static void sub()
-	{
-		
-	}
-}
---
+++ src/test/java/practice/Subtraction.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class Subtraction {
+
+	public static void main(String[] args) {
+		
+		int result = sub(67,58);
+		System.out.println(result);
+	}
+	public static int sub(int a, int b)
+	{
+		return a-b;
+	}
+	
+}
+
